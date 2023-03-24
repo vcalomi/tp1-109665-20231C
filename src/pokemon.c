6ae3fd6b96ea79f803810ff2ed58d0d94364c05f
root <root@DESKTOP-OM63Q7N>
2023-03-24 01:00:12 -0300

    Logica de funciones creada (falta probar)

--- src/pokemon.c
+++ src/pokemon.c
@@ -10,34 +10,61 @@ pokemon_t *pokemon_crear_desde_string(const char *string)
 
 pokemon_t *pokemon_copiar(pokemon_t *poke)
 {
-	return NULL;
+	if (poke == NULL) // chequeo que el puntero recibido no sea null
+		return NULL;
+
+	pokemon_t *copia_poke = malloc(sizeof(
+		pokemon_t)); // reservo memoria para la copia y me fijo que no haya fallado
+	if (copia_poke == NULL)
+		return NULL;
+
+	copia_poke =
+		poke; // guardo el pokemon en la copia, quizas hay que usar la otra funcion (mcmcpy algo asi)
+	return copia_poke; // devuelvo la copia
 }
 
 bool pokemon_son_iguales(pokemon_t *pokemon1, pokemon_t *pokemon2)
 {
-	return true;
+	if (pokemon1 == NULL || pokemon2 == NULL)
+		return NULL;
+
+	if (pokemon1->id == pokemon2->id &&
+	    pokemon1->nombre == pokemon2->nombre)
+		return true;
+	return false; // quizas hay que chequear la salud y los entrenadores tambien
 }
 
 char *pokemon_nombre(pokemon_t *pokemon)
 {
-	return 0;
+	if (pokemon == NULL)
+		return NULL;
+	return pokemon
+		->nombre; // si el puntero recibido es valido devuelvo el nombre
 }
 
 char *pokemon_entrenador(pokemon_t *pokemon)
 {
-	return 0;
+	if (pokemon == NULL)
+		return NULL;
+
+	return pokemon->nombre_entrenador;
 }
 
 size_t pokemon_salud(pokemon_t *pokemon)
 {
-	return 0;
+	if (pokemon == NULL)
+		return NULL;
+	return pokemon->salud;
 }
 
 size_t pokemon_id(pokemon_t *pokemon)
 {
-	return 0;
+	if (pokemon == NULL)
+		return NULL;
+	return pokemon->id;
 }
 
 void pokemon_destruir(pokemon_t *pkm)
 {
+	free(pkm);
 }
--- src/tp1.c
+++ src/tp1.c
@@ -19,28 +19,64 @@ hospital_t *hospital_crear_desde_archivo(const char *nombre_archivo)
 
 size_t hospital_cantidad_pokemones(hospital_t *hospital)
 {
-	return 0;
+	return hospital->cantidad_pokemon; //devolver cantidad pokemon?
 }
 
 size_t hospital_a_cada_pokemon(hospital_t *hospital,
 			       bool (*funcion)(pokemon_t *p, void *aux),
 			       void *aux)
 {
-	return 0;
+	size_t cant_veces_invocada = 0;
+	size_t i = 0;
+	while (i < hospital->cantidad_pokemon) {
+		if (funcion(hospital->pokemones[i], aux) == false)
+			return cant_veces_invocada;
+		cant_veces_invocada++;
+		i++;
+	}
+
+	return cant_veces_invocada;
+	// creo un contador para el limite de iteracion y uno para la cantidad de veces que se aplico la funcion, si la funcion es false con algun pokemon
+	// devuelvo el contador, sino sigo aumentando el contador de la funcion y el de la iteracion hasta que finalice y devuelvo el contador de la funcion nuevamente.
 }
 
 int hospital_aceptar_emergencias(hospital_t *hospital,
 				 pokemon_t **pokemones_ambulancia,
 				 size_t cant_pokes_ambulancia)
 {
-	return ERROR;
+	hospital->pokemones =
+		realloc(hospital->pokemones,
+			sizeof(pokemones_ambulancia) *
+				cant_pokes_ambulancia); //esto es raro, fijarse
+
+	if (hospital->pokemones == NULL)
+		return ERROR; // fallo realloc
+
+	for (size_t i = hospital->cantidad_pokemon;
+	     i < cant_pokes_ambulancia + hospital->cantidad_pokemon; i++) {
+		hospital->pokemones[i] =
+			pokemones_ambulancia[i - hospital->cantidad_pokemon];
+		// funcion itera desde la ultima posicion del vector de pokemones hasta la ultima nueva posicion posible, ej:
+		// si tengo 7 pokemones en hospital e ingresan 3 pokemones, i arranca en 7 y va hasta 10. y agrego en la posicion de i
+		// el pokemon en la posicion i-los pokemones que habia de los que hay en la ambulancia.
+		// quizas hay que ordenar los pokemones ingresados
+	}
+
+	return EXITO;
 }
 
 pokemon_t *hospital_obtener_pokemon(hospital_t *hospital, size_t prioridad)
 {
-	return NULL;
+	if ((hospital->pokemones[0]) == NULL)
+		return NULL;
+	return hospital->pokemones[0]; // devolver pokemon en posicion cero?
 }
 
 void hospital_destruir(hospital_t *hospital)
 {
+	for (size_t i = 0; i < hospital->cantidad_pokemon; i++) {
+		free(hospital->pokemones[i]);
+	}
+	free(*hospital->pokemones);
+	free(hospital); // liberar cada pokemon, luego el vector de pokemones y luego el hospital?
 }
